e77ec05f2067b96ff5682446e3deab098ba184ae
MaiLakshyaHoon <lakshyamaharshi007@gmail.com>
2024-07-22 01:03:56 +0530

    Work component structure created

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,17 +10,17 @@ const About = () => {
       <div className="ml-28 my-24 " >
         <ul className="list-disc" >
           <li className="my-16" >
-            <h4 className="mb-4" >June - Sept. (2024)</h4>
+            <h4 className="mb-4" >June - Sept. (2024) {`{Frountend Developer Intern}`} </h4>
             <p className="ml-4" >As a paid Frontend Developer Intern at JPR Systems Pvt. Ltd., I leverage my skills in React and React Native to create dynamic, responsive user interfaces. I specialize in utilizing GSAP and Three.js for advanced animations, enhancing user engagement and experience. My role involves collaborating with cross-functional teams, ensuring seamless integration of frontend elements, and contributing to the development of innovative web and mobile applications.</p>
           </li>
 
           <li className="my-16" >
-            <h4 className="mb-4" >2023 - Present</h4>
+            <h4 className="mb-4" >2023 - Present {`{Freelancing}`} </h4>
             <p className="ml-4" >I am actively engaged in freelancing to enhance and refine my skills. By taking on diverse projects, I continuously challenge myself to apply and expand my expertise in web development. This hands-on experience allows me to stay updated with the latest industry trends and technologies, ensuring I deliver high-quality work to my clients. Freelancing provides me with the flexibility to explore innovative solutions and improve my problem-solving abilities, ultimately contributing to my professional growth.</p>
           </li>
 
           <li className="my-16" >
-            <h4 className="mb-4" >2023 - Present</h4>
+            <h4 className="mb-4" >2023 - Present {`{Open Source contribution}`} </h4>
             <p className="ml-4" >I am actively contributing to open source projects to enhance and refine my skills. This involvement allows me to collaborate with a global community of developers, tackle real-world problems, and gain exposure to diverse coding practices. By participating in these projects, I continuously improve my technical abilities, stay updated with the latest industry trends, and give back to the developer community. This hands-on experience is invaluable for my professional growth and helps me stay at the forefront of technology.</p>
           </li>
 
---
+++ src/components/Work.jsx
@@ -0,0 +1,34 @@
+const Work = () => {
+  return (
+    <div className="mx-28 my-16" >
+      <h1>Work.</h1>
+      <p>
+        I’ve developed over 100 projects, enhancing my skills in various
+        technologies and techniques. This diverse portfolio includes everything
+        from simple web apps to complex interfaces, showcasing my growth in
+        React, React Native, GSAP, and Three.js, and my dedication to
+        innovation.
+      </p>
+
+      <div className="workCollection flex mt-10 gap-10 ">
+
+        <div className="container1">
+          <img src="" alt="" />
+          <h5>Dec. 2023</h5>
+          <h2>Bharat Bazzar</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem magni optio cumque ea quia deserunt vitae ipsam pariatur sequi consectetur numquam amet, sunt voluptatibus consequatur velit debitis quae commodi nemo?</p>
+        </div>
+
+        <div className="container2">
+          <img src="" alt="" />
+          <h5>Dec. 2023</h5>
+          <h2>Other project</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde magni at deleniti modi voluptates id fuga numquam reiciendis quaerat sequi! Repellat repudiandae in officia exercitationem? Dignissimos praesentium incidunt unde dolorem.</p>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Work;
